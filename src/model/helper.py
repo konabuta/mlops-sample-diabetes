54a3d1f5eb2fccccce0acfbe137d192904796655
Keita Onabuta <keonabut@microsoft.com>
2022-08-11 17:42:17 +0900

    init

---
+++ src/model/helper.py
@@ -0,0 +1,13 @@
+import glob
+import os
+
+import pandas as pd
+
+
+def get_csvs_df(path):
+    if not os.path.exists(path):
+        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+    csv_files = glob.glob(f"{path}/*.csv")
+    if not csv_files:
+        raise RuntimeError(f"No CSV files found in provided data path: {path}")
+    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
--- src/model/train.py
+++ src/model/train.py
@@ -1,12 +1,9 @@
-from sklearn.model_selection import train_test_split
-
 import argparse
-import glob
-import os
 
 import mlflow
-import pandas as pd
+from helper import get_csvs_df
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
@@ -24,16 +21,7 @@ def main(args):
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
-def get_csvs_df(path):
-    if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-    csv_files = glob.glob(f"{path}/*.csv")
-    if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}")
-    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
-
-
-# TO DO: add function to split data
+# split data
 def split_data(df):
     X, y = (
         df[
